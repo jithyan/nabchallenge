67aed80c8e9b6bf8b1024e4dffc4fb61e5fddc12
moody-jnan <jithya.n@outlook.com>
2019-10-18 00:34:29 +1100

    working on selector component stub

--- nab-challenge-ui/src/components/Selector/Selector.tsx
+++ nab-challenge-ui/src/components/Selector/Selector.tsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Selector: React.FC = () => {
+  const [currencyNames, setCurrencyNames] = useState<string[]>([] as string[]);
+  const [currentValue, setCurrentValue] = useState<string>("");
+
+  const onValueSelected = (event: React.FormEvent<HTMLFormElement>) => {
+    setCurrentValue(event.currentTarget.value);
+  };
+
   return (
     <div className="ui raised very padded text container segment">
-      <h1>Pick a crypto currency</h1>
-      <h3>ETH</h3>
-      <button>Next</button>
+      <CurrencySelector
+        currencyNames={[]}
+        onCurrencySelected={onValueSelected}
+      />
+      {currentValue !== "" ? <button>Next</button> : null}
+      <h1>{currentValue}</h1>
+    </div>
+  );
+};
+
+interface CurrencySelectorProps {
+  currencyNames: string[];
+  onCurrencySelected: (event: any) => void;
+}
+const CurrencySelector: React.FC<CurrencySelectorProps> = ({
+  currencyNames,
+  onCurrencySelected
+}) => {
+  const isLoading = currencyNames.length === 0;
+  const options = currencyNames.map(name => (
+    <option key={name} value={name}>
+      {name}
+    </option>
+  ));
+
+  return isLoading ? (
+    <Loader text="Retrieving currencies" />
+  ) : (
+    <form className="ui form">
+      <select
+        onChange={onCurrencySelected}
+        className="ui dropdown"
+        placeholder="Pick a value"
+      >
+        <option value="" key="">
+          Pick a currency
+        </option>
+        {options}
+      </select>
+    </form>
+  );
+};
+
+interface LoaderProp {
+  text: string;
+}
+const Loader: React.FC<LoaderProp> = ({ text = "Loading" }) => {
+  return (
+    <div>
+      <div className="ui active loader">{text}</div>
     </div>
   );
 };
