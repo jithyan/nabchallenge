030710596526c14cc322ea8896986cf59aee75e8
moody-jnan <jithya.n@outlook.com>
2019-10-19 23:29:27 +1100

    Added schema validation. Fixed errors in date representation and data generation

--- db/gendata/index.ts
+++ db/gendata/index.ts
@@ -4,46 +4,45 @@ import * as fs from "fs";
 const cryptos = ["LTC", "BTC", "ETC"];
 const dates: Date[] = [];
 
-var date = new Date(2017, 0);
+const date = new Date(2017, 0);
 for (var i = 0; i < 365; i++) {
-    dates.push(date);
-    date = moment(date)
-        .add(1, "d")
-        .toDate();
+  dates.push(
+    moment(date)
+      .add(i, "d")
+      .toDate()
+  );
 }
 
-type Quote = { time: number, price: number };
-type Doc = { currency: string, date: Date, quotes: Quote[] };
+type Quote = { time: number; price: number };
+type Doc = { currency: string; date: number; quotes: Quote[] };
 
 function gen_quotes(date: Date): Quote[] {
-    const quotes: Quote[] = [];
-    const dayInMins = 60 * 24;
-    var time = date;
+  const quotes: Quote[] = [];
+  const dayInMins = 60 * 24;
+  var time = date.getTime();
 
-    for (var i = 0; i < dayInMins; i++) {
-        const price = parseFloat((Math.random() * 100).toFixed(2));
-        const unixTime = parseInt(Math.floor(time.getTime() / 1000).toFixed(0));
-        time = moment(date)
-            .add(1, "m")
-            .toDate();
-        quotes.push({
-            time: unixTime,
-            price
-        })
-    }
-    return quotes;
+  for (var i = 0; i < dayInMins; i++) {
+    const price = parseFloat((Math.random() * 100).toFixed(2));
+    quotes.push({
+      time,
+      price
+    });
+    time = time + 60000;
+  }
+  return quotes;
 }
 
 const docs: Doc[] = [];
-cryptos.forEach(function (currency) {
-    dates.forEach(function (date) {
-        const doc = {
-            currency,
-            date,
-            quotes: gen_quotes(date)
-        };
-        docs.push(doc);
-    });
+cryptos.forEach(function(currency) {
+  dates.forEach(function(date) {
+    const doc = {
+      currency,
+      date: date.getTime(),
+      quotes: gen_quotes(date)
+    };
+    docs.push(doc);
+  });
 });
 
+console.log(docs.length);
 fs.writeFileSync("./cryptoprices.json", JSON.stringify(docs));
--- db/setup.js
+++ db/setup.js
@@ -1,6 +1,36 @@
 conn = new Mongo();
 db = conn.getDB("NAB");
 
-db.createCollection("cryptoPrices");
+db.createCollection("cryptoPrices", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        currency: {
+          bsonType: "string"
+        },
+        date: {
+          bsonType: "date"
+        },
+        quotes: {
+          bsonType: "array",
+          items: {
+            $jsonSchema: {
+              bsonType: "object",
+              properties: {
+                time: {
+                  bsonType: "date"
+                },
+                price: {
+                  bsonType: "decimal"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+});
 
 db.cryptoPrices.createIndex({ currency: 1, date: -1 });
